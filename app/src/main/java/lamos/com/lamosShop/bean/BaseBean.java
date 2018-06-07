8febec53521f5d3d79297dc220a533781ed98f74
Lamos <msm8274ac@163.com>
2018-06-07 21:30:43 +0800

    First commit.

---
+++ app/src/main/java/lamos/com/lamosShop/bean/BaseBean.java
@@ -0,0 +1,18 @@
+package lamos.com.lamosShop.bean;
+
+import java.io.Serializable;
+
+
+public class BaseBean implements Serializable {
+
+
+    protected   long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
